56322a17777d3cbc99586de34585e3f706838a06
Zameer Shaik <zameershaik@Zameers-MBP.lan>
2023-08-26 19:07:50 -0400

    two sum leet code

---
+++ twosum.py
@@ -0,0 +1,17 @@
+def two_sum(nums, target):
+    num_to_index = {}  # Dictionary to store numbers and their indices
+    
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in num_to_index:
+            return [num_to_index[complement], index]
+        num_to_index[num] = index
+    
+    # If no solution is found, return an empty list or raise an exception
+    return []
+
+# Example usage
+nums = [2, 7, 11, 15]
+target = 9
+result = two_sum(nums, target)
+print(result) 
